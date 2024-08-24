64163078332e276dae0d240d3e67982680bc8e57
rain-gayming <raingayming@gmail.com>
2024-08-24 20:36:49 +0100

    basic inventory ui - morrowind-esc

---
+++ rpg/inventory/scripts/container.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63cbf485c8ff1d37fe1b2437b6de65b418314a6b7254c8145bfe34dd7b0c7359
+size 260
---
+++ rpg/inventory/scripts/inventory_ui.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0fe7a3734ae4491db67059863553a8deba776f9bf5c26de6cd309239a096f23
+size 493
