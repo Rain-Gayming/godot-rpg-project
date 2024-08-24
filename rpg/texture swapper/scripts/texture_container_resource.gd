0de8f693aa60e819675d073bcb4008445865cfd1
rain-gayming <raingayming@gmail.com>
2024-08-24 13:41:14 +0100

    texture settings and default settings

---
+++ rpg/texture swapper/scripts/texture_container_resource.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:26f7d111a82dd52143fa139b60ea15eed0875f0cff96003733d7c91c9708d792
+size 146
--- rpg/ui/scripts/options_menu.gd
+++ rpg/ui/scripts/options_menu.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb9c28d6d4b5461dc763a88d58b075cf6a451e4d194f96dd951065dd037fa2d9
-size 5958
+oid sha256:fa7e896d0e4e2c193ab956c8a80d71517e96055d83654b03826c3c97cc61e6ea
+size 6895
